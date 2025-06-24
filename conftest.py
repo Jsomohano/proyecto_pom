7d8ffec46282f1c9394be5d295a1fbe050d13c01
Jaime Somohano <jaimesomohano@gmail.com>
2025-06-24 17:33:45 -0600

    Update Chrome WebDriver initialization in conftest.py to use Service and ChromeDriverManager; remove cached bytecode file.

--- conftest.py
+++ conftest.py
@@ -1,12 +1,14 @@
 # conftest.py
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from pages.home_page import HomePage
 from pages.sing_in_page import SignInPage
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     yield driver
     driver.quit()
 
