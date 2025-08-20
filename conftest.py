66f8cf67af21193271968f2026fc79351c2f6507
Jaime Somohano <jaimesomohano@gmail.com>
2025-08-20 17:11:56 -0600

    Fixing CI issues

--- conftest.py
+++ conftest.py
@@ -1,35 +1,51 @@
 # conftest.py
 import pytest
+import tempfile
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+import os
+
 from pages.home_page import HomePage
 from pages.sing_in_page import SignInPage
 
-@pytest.fixture
+# Importa ChromeDriverManager solo si no estamos en GitHub Actions
+if not os.environ.get("GITHUB_ACTIONS"):
+    from selenium.webdriver.chrome.service import Service
+    from webdriver_manager.chrome import ChromeDriverManager
+
+
+@pytest.fixture(scope="function")
 def driver():
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    chrome_options = Options()
+
+    # Opciones para ejecución en cualquier entorno
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+
+    if os.environ.get("GITHUB_ACTIONS"):
+        # Configuración específica para GitHub Actions
+        chrome_options.add_argument("--headless")
+        user_data_dir = tempfile.mkdtemp()
+        chrome_options.add_argument(f"--user-data-dir={user_data_dir}")
+        driver = webdriver.Chrome(options=chrome_options)
+    else:
+        # Configuración para ejecución local
+        service = Service(ChromeDriverManager().install())
+        driver = webdriver.Chrome(service=service, options=chrome_options)
+
     yield driver
     driver.quit()
 
 @pytest.fixture
 def home_page(driver):
-    """Fixture que proporciona una instancia de HomePage ya configurada"""
     return HomePage(driver)
 
 @pytest.fixture
 def sign_in_page(driver):
-    """Fixture que proporciona una instancia de SignInPage ya configurada"""
     return SignInPage(driver)
 
 @pytest.fixture
 def pages(driver):
-    """
-    Fixture que proporciona todas las páginas instanciadas en un solo objeto.
-    Uso: def test_example(pages):
-        pages.home.navigate_to_home_page()
-        pages.sign_in.enter_sign_in_info(email, password)
-    """
     class Pages:
         def __init__(self, driver):
             self.home = HomePage(driver)
