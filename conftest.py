b7c4b22cf5acb43a0410767f379efa865bbc6e9f
Jaime Somohano <jaimesomohano@gmail.com>
2025-08-20 16:16:34 -0600

    Enhance Chrome WebDriver setup in conftest.py to support both local and CI environments with improved options for ChromeDriver initialization.

--- conftest.py
+++ conftest.py
@@ -5,10 +5,28 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from pages.home_page import HomePage
 from pages.sing_in_page import SignInPage
+from selenium.webdriver.chrome.options import Options
+
+import os
+
+# Solo importa ChromeDriverManager si lo vas a usar
+if not os.environ.get("GITHUB_ACTIONS"):
+    from selenium.webdriver.chrome.service import Service
+    from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    chrome_options = Options()
+    # chrome_options.add_argument("--headless")  # Opcional
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+
+    if os.environ.get("GITHUB_ACTIONS"):
+        # En CI, ChromeDriver ya está en el PATH
+        driver = webdriver.Chrome(options=chrome_options)
+    else:
+        # Localmente, usa ChromeDriverManager
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
     yield driver
     driver.quit()
 
